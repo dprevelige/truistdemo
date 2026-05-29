c4be2ede9d1a9e989f374349b7fe6d787a197aed
Doug Prevelige <dpreveli@adobe.com>
2026-05-28 17:11:21 -0700

    feat: migrate truist-confidence-savings

---
+++ blocks/feature-list/feature-list.js
@@ -0,0 +1,5 @@
+export default async function decorate(block) {
+  // The block structure is already correct:
+  // Each row contains cells, each cell has a value (strong) and description
+  // No additional DOM manipulation needed
+}
